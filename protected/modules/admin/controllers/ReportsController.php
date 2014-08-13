d34a6c531e8733371fbcebf729fbcc189762ad91
Ahmad Suaidi <suaidi32@gmail.com>
2014-08-13 13:49:36 +0700

    report controller

--- protected/modules/admin/controllers/ReportsController.php
+++ protected/modules/admin/controllers/ReportsController.php
@@ -3,29 +3,6 @@ require_once(Yii::app()->basePath . '/vendor/jasper/Jasper/Jasper.php');
 
 class ReportsController extends Controller {
     public function actionIndex() {
-        
-        /* curl login to jasperserver */
-//        $url = 'http://localhost:8080/jasperserver/j_spring_security_check';
-//        $params = array(
-//            'j_username' => 'jasperadmin',
-//            'j_password' => 'jasperadmin',
-//            'j_password_pseudo' => 'jasperadmin',
-//            'userLocale' => 'en_US',
-//            'userTimezone' => 'Asia/Bangkok',
-//            'j_acegi_security_check' => ''
-//        );
-//        Yii::app()->curl->post($url, $params);
-//        
-//        $out = Yii::app()->curl->setOption(CURLOPT_HEADER, true)->post('http://localhost:8080/jasperserver/flow.html?_flowId=viewReportFlow&standAlone=true&_flowId=viewReportFlow&ParentFolderUri=/reports&reportUnit=/reports/RMLubang&decorate=no&j_acegi_security_check?',$params);
-//        
-//        echo '<iframe src="'.$out.'"></iframe>';
-//        die();
-        
-        //Yii::import('application.vendor.folder.TestCoba');
-        
-        //TestCoba::coba();
-        
-        
 		$reports_raw = Report::model()->findAll();
 		$reports = Report::reportItems($reports_raw);
         
