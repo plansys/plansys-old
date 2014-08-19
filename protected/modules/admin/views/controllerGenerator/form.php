2e3137679d735fb51103aa262cba9cbadaac0c21
Teguh <teguh.satriya.w@gmail.com>
2014-08-19 15:12:00 +0700

    controllerGenerator

--- protected/modules/admin/views/controllerGenerator/form.php
+++ protected/modules/admin/views/controllerGenerator/form.php
@@ -11,8 +11,8 @@
                         <td><span class="code"><?php echo Yii::getPathOfAlias($class).'.php';?></span></td>
                     </tr>
                 </table>
-                <div class="action-list">
-                    <h4>Action List</h4>
+                <div ui-header>Action List</div>
+                <div class="action-list">   
                     <?php if(empty($method)):?>
                         <div class="alert alert-warning">
                             <span>Action Empty</span>
@@ -21,14 +21,14 @@
                         <table class="table-responsive table table-bordered">
                             <thead>
                             <tr colspan=2 style="background:#f6f6f6;">
-                                <th>Action Name</th><th>Parameter</th><th></th>
+                                <th>Action Name</th><th colspan="2">Parameter</th>
                             </tr>
                             </thead>
                             <tbody>
                             <?php foreach ($method as $m):?>
                             <tr>
-                                <td><?php echo $m['name'];?></td>
-                                <td><?php 
+                                <td style="width:45%;"><?php echo $m['name'];?></td>
+                                <td style="width:45%;border-right-color:#ffffff;"><?php 
                                     if(!empty($m['param'])){
                                         foreach($m['param'] as $param){
                                             echo $param->name.'<br>';
@@ -38,14 +38,22 @@
                                     }
                                     ?>
                                 </td>
-                                <td></td>
+                                <td style="width:10%">
+                                    <a href="#" class="glyphicon glyphicon-eye-open"/>&nbsp;
+                                    <a href="#" class="glyphicon glyphicon-pencil"/>
+                                </td>
                             </tr>
                             <?php endforeach;?>
                             </tbody>
                         </table>
                     <?php endif;?>
                 </div>
-                
+                <div ui-header>Access Role</div>
+                <div class="action-list">
+                    <div class="alert alert-warning">
+                        <span>Access Role Empty</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -53,82 +61,6 @@
 </div>
 <script type="text/javascript">
     app.controller("PageController", ["$scope", "$http", "$timeout", function($scope, $http, $timeout) {
-            $scope.list = <?php echo CJSON::encode($method); ?>;
-            $scope.isDragged = false;
-            $scope.isLoading = true;
-            $scope.listOptions = {
-                dragStop: function(node) {
-                    if ($scope.isDragged) {
-                        $scope.save();
-                        $scope.isDragged = false;
-                    }
-                },
-                dragStart: function(node) {
-                    $scope.isDragged = true;
-                },
-                beforeDrag: function(node) {
-                    $scope.select(node.$handleScope);
-                    return true;
-                }
-            };
-            $scope.activeTree = null;
-            $scope.active = null;
-            $scope.saving = false;
-            $scope.selecting = false;
-            $scope.select = function(item) {
-                $scope.selecting = true;
-                $scope.active = null;
-                $timeout(function() {
-                    $scope.active = item.$modelValue;
-                    if (typeof $scope.active.state == "undefined") {
-                        $scope.active.state = "";
-                    }
-
-                    $scope.activeTree = item;
-                    $('#AdminMenuEditor\\[label\\]').focus().select();
-                }, 0);
-            };
-            $scope.iconAvailable = function(item) {
-                if (typeof item.icon == "undefined")
-                    return false;
-                else
-                    return (item.icon != '');
-            }
-            $scope.isCollapsed = function(item) {
-                return item.state == 'collapsed' ? true : false;
-            }
-            $scope.isSelected = function(item) {
-                if (item.$modelValue === $scope.active) {
-                    return "active";
-                } else {
-                    return "";
-                }
-            };
-            $scope.new = function() {
-                $scope.list.push({
-                    'label': 'New Menu',
-                    'icon': '',
-                    'url': '#',
-                    'items': [],
-                });
-                $scope.save();
-            }
-            $scope.remove = function(item) {
-                item.remove();
-                $scope.active = null;
-                $scope.save();
-            }
-            $scope.save = function() {
-                $scope.saving = true;
-                $http.post('<?php //echo $this->createUrl("save", array('class' => $path)); ?>',
-                        {list: $scope.list})
-                        .success(function(data, status) {
-                            $scope.saving = false;
-                        })
-                        .error(function(data, status) {
-                            $scope.saving = false;
-                        });
-            }
         }
     ]);
 </script>
