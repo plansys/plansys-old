cb265ba568cd2e791f96a6b9e1beb4c0e1a09a07
Rizky Ramadhan <Rizky>
2014-11-05 11:01:15 +0700

    fixing ng-grid

--- components/utility/Asset.php
+++ components/utility/Asset.php
@@ -4,10 +4,10 @@ class Asset extends CComponent {
 
     public static function publish($path) {
         if (strpos($path, Yii::getPathOfAlias('webroot')) == 0) {
-            $path = Yii::app()->baseUrl . '/' . substr($path, strlen(Yii::getPathOfAlias('webroot')));
+            $path = Yii::app()->baseUrl . substr($path, strlen(Yii::getPathOfAlias('webroot')));
         }
         $result = str_replace("\\", "/", $path);
         return $result;
     }
-    
+
 }
