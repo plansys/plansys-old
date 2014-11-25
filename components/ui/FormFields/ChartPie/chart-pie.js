f7f3402eccd7317cdb166aeaee6b9d4a9584e8aa
andriepu <andriepu@gmail.com>
2014-11-25 12:12:34 +0700

    update chart-pie

--- components/ui/FormFields/ChartPie/chart-pie.js
+++ components/ui/FormFields/ChartPie/chart-pie.js
@@ -86,7 +86,7 @@ app.directive('psChartPie', function ($timeout) {
 						
 						if($scope.series != null) {
 							for(i in $scope.series) {
-								$scope.series[i] = angular.extend($scope.series[i], chartData_raw[$scope.series[i].label]);
+								$scope.series[i] = angular.extend(chartData_raw[$scope.series[i].label], $scope.series[i]);
 							}
 							
 							chartData_raw = $scope.series;
