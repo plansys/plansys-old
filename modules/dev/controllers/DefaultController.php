f669f2ef8d658f6d053e59f9b41dba0105045e74
Rizky <rizky05@gmail.com>
2015-02-09 11:27:05 +0700

    fixing adminer refresh

--- modules/dev/controllers/DefaultController.php
+++ modules/dev/controllers/DefaultController.php
@@ -15,15 +15,26 @@ class DefaultController extends Controller {
         if (Yii::app()->user->isGuest) {
             throw new CHttpException(404);
         }
-        
+
         $db = Setting::get('db');
-        
-        $_GET['s'] = $db['server'];
-        $_GET['u'] = $db['username'];
-        $_GET['p'] = $db['password'];
-        $_GET['db'] = $db['dbname'];
-        
-        $this->render("adminer");
+        $params = [];
+        if (count($_GET) > 1) {
+            foreach ($_GET as $g => $i) {
+                if ($g != "r") {
+                    $params[] = $g . "=" . $i;
+                }
+            }
+            $params = implode("&", $params);
+        } else {
+            $_GET['s'] = $db['server'];
+            $_GET['u'] = $db['username'];
+            $_GET['p'] = $db['password'];
+            $_GET['db'] = $db['dbname'];
+            $params = "username={$_GET['u']}&db={$_GET['db']}&p={$_GET['p']}&s{$_GET['s']}";
+        }
+        $this->render("adminer", [
+            'params' => $params
+        ]);
     }
 
 }
--- modules/dev/views/default/adminer.php
+++ modules/dev/views/default/adminer.php
@@ -1,2 +1,22 @@
-<iframe src="plansys/adminer.php?username=<?= @$_GET['u'] ?>&db=<?= @$_GET['db'] ?>&p=<?= @$_GET['p']; ?>&s<?= $_GET['s'] ?>" frameborder="0" 
+<iframe src="plansys/adminer.php?<?= $params ?>" frameborder="0" 
         style="position:absolute;top:0px;left:0px;right:0px;bottom:0px;width:100%;height:100%;"></iframe>
+<script type="text/javascript">
+    $(function () {
+        var def = window.location.href;
+        function replaceQueryParam(param, newval, search) {
+            var regex = new RegExp("([?;&])" + param + "[^&;]*[;&]?")
+            var query = search.replace(regex, "$1").replace(/&$/, '')
+            return (query.length > 2 ? query + "&" : "?") + param + "=" + newval
+        }
+        var def = window.location.href.split("dev/default/adminer")[0] + "dev/default/adminer";
+        var init = false;
+
+        $("iframe").load(function () {
+            if (init) {
+                var url = $("iframe")[0].contentWindow.location.href.split('?').pop();
+                history.pushState(null, "Plansys Adminer", def + "&" + url);
+            }
+            init = true;
+        });
+    });
+</script>
