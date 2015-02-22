e1babd1484c9f34842b46b389d6aab83aebe0836
Rizky <rizky05@gmail.com>
2015-02-23 06:17:14 +0700

    adding installer ctrl

---
+++ controllers/InstallController.php
@@ -0,0 +1,35 @@
+<?php
+
+class InstallController extends Controller {
+
+    public $layout = "//install/_layout";
+
+    public function filters() {
+        return array('accessControl');
+    }
+
+    public function accessRules() {
+        return array(
+            array('allow',
+                'expression' => '!Setting::plansysInstalled()',
+            ),
+            array('deny'),
+        );
+    }
+
+    public function beforeAction($action) {
+        parent::beforeAction($action);
+
+        $baseUrl = Yii::app()->baseUrl;
+        $cs = Yii::app()->getClientScript();
+        $path = str_replace([Setting::getBasePath(), "\\"], ["", "/"], $this->getViewPath());
+        $cs->registerCssFile(Yii::app()->baseUrl . $path . '/install.css');
+
+        return true;
+    }
+
+    public function actionIndex() {
+        $this->render("index");
+    }
+
+}
