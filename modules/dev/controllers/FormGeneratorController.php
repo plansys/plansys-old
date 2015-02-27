b95a965eacd358f5b18b21832288d6b4f439d90d
teguh <teguh.satriya.w@gmail.com>
2015-02-27 14:36:55 +0700

    fixing  user index

---
+++ modules/dev/controllers/FormGeneratorController.php
@@ -0,0 +1,20 @@
+<?php
+
+class FormGeneratorController extends Controller {
+
+    public function actionIndex() {
+        $templates = FormGenerator::listTemplates();
+        $this->renderForm('DevGeneratorIndex', [
+            'templates' => $templates
+        ]);
+    }
+
+    public function actionWizard($id) {
+        Yii::import('application.components.codegen.templates.' . $id . '.*');
+        $class = ucfirst($id) . "Generator";
+        $wizard = new $class;
+
+        $this->renderForm($wizard->form);
+    }
+
+}
