3443c9455acb7a36b95619f1b05a4ea5d9595644
Rizky <rizky05@gmail.com>
2015-03-01 09:11:19 +0700

    fixing context Menu

--- components/utility/Asset.php
+++ components/utility/Asset.php
@@ -2,9 +2,21 @@
 
 class Asset extends CComponent {
 
-    public static function publish($path, $extension = "", $isAlias = false) {
+    public static function publish($path, $isAlias = false) {
         if ($isAlias) {
-            $path = Yii::getPathOfAlias($path) . $extension;
+            $pathArr = explode(".", $path);
+            if (in_array($pathArr[count($pathArr) - 1], ["php", "js", "css"])) {
+                $ext = array_pop($pathArr);
+
+                $pathNew = Yii::getPathOfAlias(implode(".", $pathArr)) . "." . $ext;
+                if (is_file($pathNew)) {
+                    $path = $pathNew;
+                } else {
+                    $path = Yii::getPathOfAlias($path);
+                }
+            } else {
+                $path = Yii::getPathOfAlias($path);
+            }
         }
 
         if (strpos($path, Yii::getPathOfAlias('webroot')) == 0) {
