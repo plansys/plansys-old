fffcdbef0c38b3e0b948b904b2e41e4f36fa1b51
teguh <teguh.satriya.w@gmail.com>
2015-04-13 14:58:29 +0700

    layout

---
+++ views/layouts/report.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    </head>
+    <body>
+        <?php echo $content; ?>
+    </body>
+</html>
