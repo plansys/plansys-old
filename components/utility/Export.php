de790454e8bdd7dd754b5d9a39592c45278df40e
root <root@s1.plansys.co>
2015-11-02 02:51:28 -0600

    + Extension Open TBS for Word and Excel

--- components/utility/Export.php
+++ components/utility/Export.php
@@ -1,16 +1,15 @@
 <?php
-class ExportWord extends CComponent {
+class Export extends CComponent {
 
     /**
-     * 
+     * For Excel and Word
      * @param string $fileNameResource namafile resourse 'moduleName.fileName'
      * @param string $fileNameResult namaFile dari result
+     * @param string $extResource extension file dari resource, eg : 'docx'
      * @param array $data use array level 1
      */
-    public static function download($fileNameResource, $fileNameResult, $data) {
-        $extResource = 'docx';
-        $extResult = 'docx';
-        $fileNameResult = $fileNameResult.'.'.$extResult;
+    public static function download($fileNameResource, $extResource,  $fileNameResult, $data) {
+        $fileNameResult = $fileNameResult.'.'.$extResource;
 
         # checking tmp directory for save result
         $assetsPath = Setting::getAssetPath();
@@ -27,11 +26,19 @@ class ExportWord extends CComponent {
             $pathResources = Yii::getPathOfAlias('app.reports.' . $fileNameResourceExplode[0]);
         }
         $pathResources.='.' . $extResource;
+        
+        if(file_exists($pathResources) != true) {
+            // throw new CDbException('The specified file cannot be found.');
+            echo ('The specified file cannot be found.');
+            exit();
+        } 
 
+        spl_autoload_unregister(array('YiiBase','autoload'));
         // Include classes
-        require_once Yii::getPathOfAlias('application.vendor.ertong.tbs.opentbs.demo.tbs_class') . '.php';
-        require_once Yii::getPathOfAlias('application.vendor.ertong.tbs.opentbs.tbs_plugin_opentbs') . '.php';
-
+        require_once Yii::getPathOfAlias('ext.ertong.tbs.opentbs.demo.tbs_class') . '.php';
+        require_once Yii::getPathOfAlias('ext.ertong.tbs.opentbs.tbs_plugin_opentbs') . '.php';
+		spl_autoload_register(array('YiiBase','autoload')); 
+        
         // prevent from a PHP configuration problem when using mktime() and date()
         if (version_compare(PHP_VERSION, '5.1.0') >= 0) {
             if (ini_get('date.timezone') == '') {
@@ -44,7 +51,8 @@ class ExportWord extends CComponent {
         $TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); // load the OpenTBS plugin
         
         $template = $pathResources;
-        $TBS->LoadTemplate($template, OPENTBS_ALREADY_UTF8); // Also merge some [onload] automatic fields (depends of the type of document).
+        
+         $TBS->LoadTemplate($template, OPENTBS_ALREADY_UTF8); // Also merge some [onload] automatic fields (depends of the type of document).
         
         # get Key of array data
         foreach (array_keys($data) as $key) {
@@ -65,6 +73,4 @@ class ExportWord extends CComponent {
             exit("File [$output_file_name] has been created.");
         }
     }
-}
-
-?>
+ }
