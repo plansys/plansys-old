91cef8b32a7aa7cd03161701c09a7d9c30a1a817
root <root@s1.plansys.co>
2016-05-17 19:04:41 +0700

    fix

--- components/ui/FormFields/TextArea/template_editor.php
+++ components/ui/FormFields/TextArea/template_editor.php
@@ -1,12 +1,11 @@
 <div class="form-group form-group-sm {{ field.layout == 'Vertical' ? 'form-vertical' : ''}}">
-    <label for="{{field.name}}" 
-           expand-attributes="field.labelOptions" 
+    <label expand-attributes="field.labelOptions" 
            ng-show="field.label != ''" 
            class="col-sm-{{field.labelWidth}} control-label"
            ng-bind-html="field.label"
            ></label>
     <div class="col-sm-{{field.fieldWidth}}">
-        <textarea id='{{field.name}}' name='{{field.name}}' rows="{{field.fieldHeight}}"
+        <textarea rows="{{field.fieldHeight}}"
                   class="form-control {{field.fieldHeight > 0 ? 'force-rows' : ''}}"
                   expand-attributes="field.options" >{{field.value}}</textarea>
     </div>
