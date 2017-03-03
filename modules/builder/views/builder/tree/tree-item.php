6c43ae01f2f6bbdadaa1a3c37f413877f553bdca
rizky <rizky05@gmail.com>
2017-03-03 09:57:14 +0700

    Fix

--- modules/builder/views/builder/tree/tree-item.php
+++ modules/builder/views/builder/tree/tree-item.php
@@ -12,7 +12,7 @@
           <i class="icon arrow fa {{ getArrow(item) }}"></i> 
           <i ng-if="item.loading" class="icon fa fa-spin fa-refresh"></i> 
           <div ng-if="!item.loading" class="icon">
-               <img ng-src="<?= Yii::app()->request->baseUrl ?>/plansys/modules/builder/views/builder/tree/icons/{{getIcon(item)}}" />
+               <img ng-src="<?= Yii::app()->request->getBaseUrl(true) ?>/plansys/modules/builder/views/builder/tree/icons/{{getIcon(item)}}" />
           </div>
      </span>
      
