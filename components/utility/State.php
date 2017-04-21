b3e88dd3dd942b3de4f497dcca2023e77e187b80
rizky <rizky05@gmail.com>
2017-04-21 11:33:13 +0700

    fix

---
+++ components/utility/State.php
@@ -0,0 +1,70 @@
+<?php
+use Thrift\Transport\TSocket;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Protocol\TCompactProtocol;
+use Thrift\Protocol\TMultiplexedProtocol;
+
+class State extends CComponent {
+     public $socket, $transport, $protocol, $client;
+     private static $sm;
+     
+     private static function _open() {
+          if (!class_exists('\svc\StateManagerClient')) {
+               include(Yii::getPathOfAlias('application.components.thrift.client.state.Types') . ".php");
+               include(Yii::getPathOfAlias('application.components.thrift.client.state.StateManager') . ".php");
+          }
+          
+          $portfile = @file_get_contents(Yii::getPathOfAlias('webroot.assets.ports') . ".txt");
+          if (is_null($portfile)) {
+              throw new Exception('Thrift Daemon is not running!'); 
+          }
+          $port = explode(":", $portfile);
+          
+          self::$sm = new State;
+          try {
+               self::$sm->socket = new TSocket('127.0.0.1', $port[0]);
+               self::$sm->transport = new TBufferedTransport(self::$sm->socket, 1024, 1024);
+               self::$sm->protocol = new TMultiplexedProtocol(new TCompactProtocol(self::$sm->transport), 'StateManager');
+               self::$sm->client = new \state\StateManagerClient(self::$sm->protocol);
+               
+               self::$sm->transport->open();
+          } catch (TException $tx) {
+               print 'TException: '.$tx->getMessage()."\n";
+          }
+     }
+     
+     private static function _close() {
+          try {
+               self::$sm->transport->close();
+          } catch (TException $tx) {
+               print 'TException: '.$tx->getMessage()."\n";
+          }
+     }
+     
+     public static function set($key, $value) {
+          self::_open();
+          
+          if (!is_string($value)) {
+               $value = json_encode($value);
+          }
+          
+          self::$sm->client->setState($key, $value);
+          self::_close();
+     }
+     
+     public static function get($key) {
+          self::_open();
+          $result = self::$sm->client->getState($key);
+          self::_close();
+          
+          return $result;
+     }
+     
+     public static function del($key) {
+          self::_open();
+          $result = self::$sm->client->deleteState($key);
+          self::_close();
+          
+          return $result;
+     }
+}
